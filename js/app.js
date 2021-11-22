cd17dc13c40fcc8e8e118345daf31131d5dd61c8
Daniel Briones <daniel.briones.perez@gmail.com>
2021-11-21 18:17:20 -0600

    Feat: Add operation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const selectOperation = document.querySelector('#operation-select');
 const labelSymbol = document.querySelector('#symbol-operation');
+const form = document.querySelector('#operation-form');
 
 const operationOptions = [
     {id: 'add',
-     symbol: '+'
+     symbol: '+',
+     operationFunction: add,
     }, 
     {id: 'sub',
      symbol: '-'
@@ -19,9 +21,32 @@ addEventListeners();
 
 function addEventListeners(){
     selectOperation.addEventListener('change', operationChange);
+    form.addEventListener('submit', performOperation);
 }
 
 function operationChange(e){
     let operationSelected = operationOptions.find(x => x.id === e.target.value);
     labelSymbol.textContent = operationSelected.symbol;
+}
+
+function performOperation(e){
+    e.preventDefault();
+    
+    const selectedOperation = selectOperation.value;
+    console.log(selectedOperation);
+
+    const firstNumberInput = document.querySelector('#first-number');
+    const secondNumberInput = document.querySelector('#second-number');
+
+    const operationChoice = operationOptions.find(x => x.id === selectedOperation);
+
+    let result = operationChoice.operationFunction(firstNumberInput.value, secondNumberInput.value);
+
+    showResult(result);
+}
+
+function showResult(result){
+    let labelResult = document.querySelector('#result');
+
+    labelResult.textContent = result;
 }
---
+++ js/math.js
@@ -0,0 +1,3 @@
+const add = (a, b) => {
+    return Number(a) + Number(b);
+}
